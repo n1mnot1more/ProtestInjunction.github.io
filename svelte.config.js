888697b40585db47a5f31e17171db225fb938fc8
n1mnot1more <josie@not1more.org>
2026-05-27 17:36:13 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,40 +1,24 @@
-import adapter from "@sveltejs/adapter-static";
+import adapterStatic from "@sveltejs/adapter-static";
 import { sveltePreprocess } from "svelte-preprocess";
 import autoprefixer from "autoprefixer";
 
-const dev = process.argv.includes("dev");
-
 const preprocess = sveltePreprocess({
 	postcss: {
-		plugins: [autoprefixer()]
+		plugins: [autoprefixer]
 	}
 });
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	compilerOptions: {
 		runes: true
 	},
-
 	preprocess,
-
 	kit: {
-		adapter: adapter({
-			pages: "docs",
-			assets: "docs",
-			fallback: "index.html",
-			strict: false
-		}),
+		adapter: adapterStatic({ strict: false })
+	}
+};
 
-		paths: {
-			base: dev ? "" : "/ProtestInjunction"
-		},
-
-		trailingSlash: "always",
-
-		prerender: {
-			handleHttpError: "warn"
-		}
+export default config;		}
 	}
 };
 
